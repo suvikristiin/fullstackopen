c482b68e5470a2ec7bf7514240cd150abd47dadc
suvikristiin <suvi.vehmaanpera@gmail.com>
2023-09-12 13:27:11 +0300

    added solution to task 1.2

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -6,6 +6,16 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <>
+      <Part part={props.parts[0]} exercise={props.exercises[0]} />
+      <Part part={props.parts[1]} exercise={props.exercises[1]} />
+      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
     <p>
       {props.part} {props.exercise}
     </p>
@@ -33,9 +43,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
       <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   );
